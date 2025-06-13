5881477499810812f930dea69b33588e1ccb0f14
Kapil Krishna Yadav <93436537+Kapilkrishnayadav@users.noreply.github.com>
2025-06-13 12:15:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,43 +1,40 @@
 const ip = document.getElementById("ip");
-      const output = document.getElementById("output");
+const output = document.getElementById("output");
 
-      function startChain() {
-        new Promise((resolve, reject) => {
-          setTimeout(() => {
-            output.innerText = ip.value;
-            resolve(Number(ip.value)); // convert string to number
-          }, 2000);
-        })
-          .then((data) => {
-            return new Promise((resolve) => {
-              setTimeout(() => {
-                output.innerText = data * 2;
-                resolve(data * 2);
-              }, 2000);
-            });
-          })
-          .then((data) => {
-            return new Promise((resolve) => {
-              setTimeout(() => {
-                output.innerText = data - 3;
-                resolve(data - 3);
-              }, 1000);
-            });
-          })
-          .then((data) => {
-            return new Promise((resolve) => {
-              setTimeout(() => {
-                output.innerText = data / 2;
-                resolve(data / 2);
-              }, 1000);
-            });
-          })
-          .then((data) => {
-            return new Promise((resolve) => {
-              setTimeout(() => {
-                output.innerText = data + 10;
-                resolve(data + 10);
-              }, 1000);
-            });
-          });
-      }
+function startChain() {
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      output.innerText = "Result: " + ip.value;
+      resolve(Number(ip.value));
+    }, 2000);
+  })
+    .then((data) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          output.innerText = "Result: " + (data * 2);
+          resolve(data * 2);
+        }, 1000); // note: Cypress test expects 1 second here, not 2
+      });
+    })
+    .then((data) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          output.innerText = "Result: " + (data - 3);
+          resolve(data - 3);
+        }, 1000);
+      });
+    })
+    .then((data) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          output.innerText = "Result: " + (data / 2);
+          resolve(data / 2);
+        }, 1000);
+      });
+    })
+    .then((data) => {
+      setTimeout(() => {
+        output.innerText = "Final Result: " + (data + 10);
+      }, 1000);
+    });
+}
