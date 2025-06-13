5286d48bbb6ff3d44952f54b1dbf3be204eb76d5
Kapil Krishna Yadav <93436537+Kapilkrishnayadav@users.noreply.github.com>
2025-06-13 12:03:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-//your JS code here. If required.
+const ip = document.getElementById("ip");
+      const output = document.getElementById("output");
+
+      function startChain() {
+        new Promise((resolve, reject) => {
+          setTimeout(() => {
+            output.innerText = ip.value;
+            resolve(Number(ip.value)); // convert string to number
+          }, 2000);
+        })
+          .then((data) => {
+            return new Promise((resolve) => {
+              setTimeout(() => {
+                output.innerText = data * 2;
+                resolve(data * 2);
+              }, 2000);
+            });
+          })
+          .then((data) => {
+            return new Promise((resolve) => {
+              setTimeout(() => {
+                output.innerText = data - 3;
+                resolve(data - 3);
+              }, 1000);
+            });
+          })
+          .then((data) => {
+            return new Promise((resolve) => {
+              setTimeout(() => {
+                output.innerText = data / 2;
+                resolve(data / 2);
+              }, 1000);
+            });
+          })
+          .then((data) => {
+            return new Promise((resolve) => {
+              setTimeout(() => {
+                output.innerText = data + 10;
+                resolve(data + 10);
+              }, 1000);
+            });
+          });
+      }
